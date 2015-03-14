46564d99209d60713ab75fbcc196f1bbc9e4e061
AlexSheer <alex_sheer@inbox.lv>
2015-03-15 00:57:27 +0200

    Update notifications_module.php

--- ucp/notifications_module.php
+++ ucp/notifications_module.php
@@ -24,6 +24,7 @@ class notifications_module
 		global $db, $user, $template, $request, $phpbb_container, $config;
 
 		$start = $request->variable('start', 0);
+		$del_all = $request->variable('del_all', false);
 		$this->u_action = ''. str_replace('-sheer-delnotifications-ucp-notifications_module&amp;mode=manage','', $this->u_action) . '&amp;mode=manage';
 
 		$phpbb_notifications = $phpbb_container->get('notification_manager');
@@ -44,20 +45,22 @@ class notifications_module
 
 		$template->assign_vars(array(
 			'TOTAL_COUNT'	=> $notifications['total_count'],
+			'S_UCP_ACTION'	=> $this->u_action,
 		));
 
-		if ($request->is_set_post('submit'))
+		if ($request->is_set_post('submit') || $del_all)
 		{
 			$mark_delete = $request->variable('mark', array(0));
-			if (!empty($mark_delete))
+			if (!empty($mark_delete) || $del_all)
 			{
 				if (confirm_box(true))
 				{
-					$sql = 'DELETE FROM ' . NOTIFICATIONS_TABLE . '
-						WHERE ' . $db->sql_in_set('notification_id', $mark_delete) . '
-						AND user_id = '. $user->data['user_id'] .'';
+					$sql_where = ($del_all) ? '' : ' ' . $db->sql_in_set('notification_id', $mark_delete) . ' AND ';
+					$sql = 'DELETE
+						FROM ' . NOTIFICATIONS_TABLE . '
+						WHERE ' . $sql_where . ' user_id = ' . $user->data['user_id'] . '';
 					$db->sql_query($sql);
-					$message = $user->lang['NOTIFICATIONS_DELETE_SUCCESS'];
+					$message = ($del_all) ? $user->lang['ALL_NOTIFICATIONS_DELETE_SUCCESS'] : $user->lang['NOTIFICATIONS_DELETE_SUCCESS'];
 					$message .= '<br /><br />' . $user->lang('RETURN_UCP', '<a href="' . $this->u_action . '">', '</a>');
 					meta_refresh(3, $this->u_action);
 					trigger_error($message);
@@ -66,15 +69,12 @@ class notifications_module
 				{
 					confirm_box(false, $user->lang['CONFIRM_OPERATION'], build_hidden_fields(array(
 						'mark'		=> $mark_delete,
+						'del_all'	=> $del_all,
 						'submit'	=> true))
 					);
 				}
 			}
 		}
-
-		$template->assign_vars(array(
-			'S_UCP_ACTION'			=> $this->u_action,
-		));
 
 		$this->tpl_name = 'ucp_delnotifications_body';
 	}
