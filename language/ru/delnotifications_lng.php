16271db2809bb4b892b12ac215c5f8df11da9895
AlexSheer <alex_sheer@inbox.lv>
2015-03-14 14:35:57 +0200

    Create delnotifications_lng.php

---
+++ language/ru/delnotifications_lng.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package phpBB Extension - Delete unnecessary notifications
+* @copyright (c) 2015 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'UCP_DELNOTIFICATIONS'				=> 'Удаление уведомлений',
+	'UCP_NOTIFICATIONS_DELETE'			=> 'Удаление ненужных уведомлений',
+	'UCP_NOTIFICATIONS_DELETE_EXPLAIN'	=> 'Здесь вы можете выбрать и удалить ненужные уведомления.',
+	'NOTIFICATIONS_DELETE_SUCCESS'		=> 'Выбранные уведомления были успешно удалены.',
+));
